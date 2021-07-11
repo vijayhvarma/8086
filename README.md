6aee7f4e5078bf6f0b2358810fb14a9b68251cfb
Vijay Varma <vijayhvarma@gmail.com>
2021-07-11 17:35:31 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# redclient
+
+This is a test and first update
+
+This is more to that and final updatre
---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+ mongoURI: 'mongodb://127.0.0.1:27017/rc',
+  secretOrKey: '/^[a-zA-Z0-9]{10}$/',
+  Field1Name: '/^[a-zA-Z0-9]{10}$/',
+  Field2Name: '/^[a-zA-Z0-9]{10}$/',
+  Secret1Key: '/^[a-zA-Z0-9]{10}$/',
+  Secret2Key: '/^[a-zA-Z0-9]{10}$/',
+  dummy:'dummy'
+};
